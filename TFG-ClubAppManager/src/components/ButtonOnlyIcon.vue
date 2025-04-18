3ac5db8ec0dea05dddb48983bdbe3f35b17a6fcc
AlvaroCuti <alvarocutillas9@gmail.com>
2025-04-18 10:31:29 +0200

    Borrar equipo hecho.

---
+++ TFG-ClubAppManager/src/components/ButtonOnlyIcon.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  icon:{
+    type: String,
+    required: true
+  },
+  color:{
+    type: String,
+    required: true
+  }
+})
+const emit = defineEmits(["click"]);
+</script>
+
+<template>
+    <button @click="emit('click')" class="boton" :style="{ backgroundColor: props.color }">
+        <img :src="icon" width="20" height="20"/>
+    </button>
+</template>
+  
+
+<style scoped>
+    .boton{
+        border-radius: 7px;
+        border: none;
+        padding-right: 10px;
+        padding-left: 10px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+
+</style>
--- TFG-ClubAppManager/src/components/EquipoInfoTag.vue
+++ TFG-ClubAppManager/src/components/EquipoInfoTag.vue
@@ -1,8 +1,11 @@
 <script setup>
     import ButtonWithIcon from '../components/ButtonWithIcon.vue';
-    import iconoPlusPerson from '@/assets/IconPersonPlus.png'; 
-
-    defineProps({
+    import trash from '@/assets/trash.png'; 
+    import ButtonOnlyIcon from '../components/ButtonOnlyIcon.vue';
+    import { useAuthStore } from '@/stores/auth'
+    const auth = useAuthStore()
+    
+    const props = defineProps({
         nombre:{
             type: String,
             required: true
@@ -24,6 +27,26 @@
             required: true
         },
     })
+    
+    const borrar = async () => {
+    try {
+      const response = await fetch(`http://localhost:8081/api/equipo/${props.nombre}`, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${auth.token}`
+        },
+      });
+
+      if (response.ok) {
+        console.log(response);
+      } else {
+        const errorData = await response.json();
+        console.error("Error:", errorData);
+      }
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+    }
+  };
 </script>
 
 <template>
@@ -33,7 +56,10 @@
         <h3>Participantes: {{participantes}}</h3>
         <h3>Entrenador: {{entrenador}}</h3> 
         
-        <ButtonWithIcon :icon="iconoPlusPerson" class="boton" placeholder="Añadir Miembro"></ButtonWithIcon>
+        <div class="botones">
+            <ButtonWithIcon :icon="iconoPlusPerson" class="boton" placeholder="Añadir Miembro"></ButtonWithIcon>
+            <ButtonOnlyIcon :icon="trash" class="boton2" color="#c70714" @click="borrar"></ButtonOnlyIcon>
+        </div>
 
     </div>
 
@@ -61,39 +87,13 @@
     font-weight: 600;
 }
 
-.tag > button{
-    margin-top: 10px;
-    margin-bottom: 7px;
-}
-
-.boton{
-    margin-left: 60px;
-    margin-right: 60px;
-}
-
-
-button {
-    border-radius: 7px;
-    background-color: #6543E0;
-    color:#F6F5F8;
-    border: 0;
+.botones{
     display: flex;
     flex-direction: row;
-    justify-content: center;
     align-items: center;
-    gap: 12px;
-    padding-right: 18px;
-    padding-left: 18px;
-    padding-top: 6px;
-    padding-bottom: 5px;
-    font-size: smaller;
-    font-weight: 700;
+    justify-content: center;
+    margin-top: 10px;
+    gap: 15px;
 }
-
-button:hover{
-  background-color: #593bc8; /* Verde más oscuro */
-  transform: scale(1.02); /* Hace el botón un poco más grande */
-}
-
 
 </style>
