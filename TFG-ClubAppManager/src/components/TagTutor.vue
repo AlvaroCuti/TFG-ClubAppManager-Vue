77acfa02e82aa588439f80924c0f6b8b4e1b7b3c
AlvaroCuti <alvarocutillas9@gmail.com>
2025-05-25 22:15:57 +0200

    Arreglo

--- TFG-ClubAppManager/src/components/TagTutor.vue
+++ TFG-ClubAppManager/src/components/TagTutor.vue
@@ -8,7 +8,7 @@ defineProps({
 
 <template>
     <div class="tutor">
-        <img alt="logo" class="logo" src="@/assets/user.png" width="25" height="25" />
+        <img alt="logo" class="logo" src="@/assets/User.png" width="25" height="25" />
         <h4>{{placeholder}}</h4>
     </div>
 </template>
